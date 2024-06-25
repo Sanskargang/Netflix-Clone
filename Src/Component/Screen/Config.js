76ccb03dbce72ae2c1070fabe65ec9f8075870fc
Sanskar Gang <94092231+Sanskargang@users.noreply.github.com>
2024-06-25 17:05:52 +0530

    Update Config.js
    Put your api id

--- Src/Component/Screen/Config.js
+++ Src/Component/Screen/Config.js
@@ -1,8 +1,8 @@
  export const firebaseConfig = {
-    apiKey: "AIzaSyBej5WLB8hJknpXr5R1mRCK7q7o7yx8lJs",
+    apiKey: "Your API KEY",
     authDomain: "netflixclone-2dc2a.firebaseapp.com",
     projectId: "netflixclone-2dc2a",
     storageBucket: "netflixclone-2dc2a.appspot.com",
     messagingSenderId: "352345571238",
-    appId: "1:352345571238:web:500911499b7ebb02fed967"
+    appId: "APP ID"
   };
